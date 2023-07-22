faf7608d7a562ca155ee8ae01cf636be1af4dbd5
Atiq717 <atiq717@hotmail.co.uk>
2023-07-22 14:40:45 +0100

    second commit

--- javascript.js
+++ javascript.js
@@ -10,25 +10,24 @@ const quote5 = '"The bad news is time flies. The good news is you arw the pilot.
 
 let quotes = [quote1, quote2, quote3, quote4, quote5]
 
-let emoji = [8987,9193,9194,9195,9196,9197,9198,9199,9200,9201,9202,9203,9208,9209,9210,9410]
+let emoji = [128568, 128569, 128570, 128571, 128572, 128573, 128574, 128575, 128576, 128577, 128578, 128579, 128580]
 
-const runQuotes = (quote) => {
-    let i = Math.floor(Math.random() * quote.length);
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    let randomEmoji = Math.floor(Math.random()* emoji.length)
-        return quotes[i] + ` #${randomColor} &#${emoji[randomEmoji]}.` 
-        // return document.getElementsByClassName('words').innerHTML = value[i];
-        // document.body.style.background = "#" + randomColor;
-        // document.body.style.color = "#" + randomColor;
-        // document.getElementsByTagName('span') =
-    }
+let element = document.querySelector('button');
+let elm = document.querySelector('div');
+let elem = document.querySelector('span');
+let randomEmoji = Math.floor(Math.random()* emoji.length)
+let randomcolor = Math.floor(Math.random()*16777215).toString(16)
+let randomSentence = Math.floor(Math.random() * quotes.length)
+let paragraph = document.createElement('p');
+function run(){
+    elm.style.backgroundColor = `#${randomcolor}`;
+    elem.style.color = `#${randomcolor}`;
+    elm.innerHTML = quotes[randomSentence]
+    paragraph.innerHTML = `&#${emoji[randomEmoji]}`
+    document.body.appendChild(paragraph)
 
-// runQuotes(quotes)
+}
 
-console.log(runQuotes(quotes))
-
-
-
-
+element.onclick = run
 
 
